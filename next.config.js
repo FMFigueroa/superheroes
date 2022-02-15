8cdf9c48d9436cffd04e3276dd24cb1167563433
Felix Figueroa <felixmanuelfigueroa@gmail.com>
2022-02-14 23:30:56 -0300

    Deploy a Vercel

--- next.config.js
+++ next.config.js
@@ -6,10 +6,8 @@ module.exports = {
   Vercel: Works automatically when you deploy on Vercel, no configuration necessary. */
 
   //note: Las imágenes no se pueden optimizar en el momento de la compilación mediante next export. 
-  /* images: {
+  images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     domains: ['res.cloudinary.com', 'www.superherodb.com', 'img.icons8.com', "png.pngitem.com", "wallpaperaccess.com"],
-  }, */
-
-
+  },
 }
